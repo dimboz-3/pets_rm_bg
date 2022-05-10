a41e6d95c7b322446482c4767641ea16515f30ae
dimboz-3 <dimboz-3@rambler.ru>
2022-05-10 10:53:55 +0300

    2

--- find_pet.py
+++ find_pet.py
@@ -11,12 +11,16 @@ from resnet_unet import ResNetUNet
 import os
 import requests
 from io import BytesIO
+import gdown
 
 @st.cache()
-def load_model(path: str = '../rm_bg/resnet_unet_rm_bg_for_pets.pth'):
+def load_model():
 	model = ResNetUNet(1)
 	
+	path = 'resnet_unet_rm_bg_for_pets.pth'
+	#gdown.download(id = "1-4eVIIfim8vf_kMm9uPMj_JVeOxdbYV2")
 	assert(os.path.exists(path))
+
 	model.load_state_dict(torch.load(path,map_location=torch.device('cpu')))
 	model.eval()
 	return model
@@ -40,7 +44,11 @@ def predict(
 	return att_mask[:,:,0]
 
 
-
+def load_var_from_session(key):
+	if key in st.session_state:
+		return  st.session_state[key]
+	else:
+		return None
 
 if __name__ == '__main__':
 
@@ -48,24 +56,38 @@ if __name__ == '__main__':
 
 	st.title('Welcome To Project find Pets on photo!')
 
-	img = None
+	img 			= load_var_from_session("img")
+	file_old 		= load_var_from_session("file_old")
+	file_url_old 	= load_var_from_session("file_url_old")
+	att_mask 		= load_var_from_session("att_mask")
+
+	NEW_IMG = False
+
 	#upload file
 	file = st.file_uploader('Upload An Image')
-	if file:
+	if file and (file_old != file):
 		img = Image.open(file)
+		file_old = file
+		NEW_IMG = True
+
 	#load by url	
 	file_url = st.text_input('image url')
-	if file_url:
+	if file_url and (file_url_old != file_url):
 		response = requests.get(file_url)
-		img = Image.open(BytesIO(response.content))
-		
-		
-	if img:
+		img = Image.open(BytesIO(response.content))	
+		file_url_old = file_url
+
+	if NEW_IMG:
+		img=np.array(img)
+		att_mask = predict(img,model)
+		st.session_state["att_mask"]=att_mask
+
+
+	if img is not None:
 		st.title("Here is the image you've selected")
 		st.image(img)
-		img=np.array(img)
+
 		st.title("Attention mask")
-		att_mask = predict(img,model)
 		st.image(Image.fromarray(np.uint8(att_mask*255)))
 		
 		att_threshold = st.slider("attention threshold",
@@ -83,5 +105,8 @@ if __name__ == '__main__':
 			img*att_mask[:,:,None]+255*(1-att_mask[:,:,None]), 255)
 		st.image(Image.fromarray(np.uint8(cut_img_smooth)))
 		
-		
+	st.session_state["img"]=img
+	st.session_state["file_old"]=file_old
+	st.session_state["file_url_old"]=file_url_old
+
 		
